545f665485591944e3aca6a55c353288d4e8efbc
03tommysad <144818027+03tommysad@users.noreply.github.com>
2024-10-10 14:00:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,27 +46,27 @@ def login_and_reserve(users, usernames, passwords, action, success_list=None):
     return success_list
 
 
-def main(users, action=False):
-    current_time = get_current_time(action)
-    logging.info(f"start time {current_time}, action {'on' if action else 'off'}")
-    attempt_times = 0
-    usernames, passwords = None, None
-    if action:
-        usernames, passwords = get_user_credentials(action)
-    success_list = None
-    current_dayofweek = get_current_dayofweek(action)
-    today_reservation_num = sum(1 for d in users if current_dayofweek in d.get('daysofweek'))
-    while current_time < ENDTIME:
-        attempt_times += 1
-        # try:
-        success_list = login_and_reserve(users, usernames, passwords, action, success_list)
-        # except Exception as e:
-        #     print(f"An error occurred: {e}")
-        print(f"attempt time {attempt_times}, time now {current_time}, success list {success_list}")
-        current_time = get_current_time(action)
-        if sum(success_list) == today_reservation_num:
-            print(f"reserved successfully!")
-            return
+# def main(users, action=False):
+#     current_time = get_current_time(action)
+#     logging.info(f"start time {current_time}, action {'on' if action else 'off'}")
+#     attempt_times = 0
+#     usernames, passwords = None, None
+#     if action:
+#         usernames, passwords = get_user_credentials(action)
+#     success_list = None
+#     current_dayofweek = get_current_dayofweek(action)
+#     today_reservation_num = sum(1 for d in users if current_dayofweek in d.get('daysofweek'))
+#     while current_time < ENDTIME:
+#         attempt_times += 1
+#         # try:
+#         success_list = login_and_reserve(users, usernames, passwords, action, success_list)
+#         # except Exception as e:
+#         #     print(f"An error occurred: {e}")
+#         print(f"attempt time {attempt_times}, time now {current_time}, success list {success_list}")
+#         current_time = get_current_time(action)
+#         if sum(success_list) == today_reservation_num:
+#             print(f"reserved successfully!")
+#             return
 
 def debug(users, action=False):
     logging.info(f"Global settings: \nSLEEPTIME: {SLEEPTIME}\nENDTIME: {ENDTIME}\nENABLE_SLIDER: {ENABLE_SLIDER}\nRESERVE_NEXT_DAY: {RESERVE_NEXT_DAY}")
