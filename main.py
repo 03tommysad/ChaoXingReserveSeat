0e78f374724c54654e2e1a81e0e8e109791589b5
03tommysad <144818027+03tommysad@users.noreply.github.com>
2025-03-05 21:35:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main(users, action=False):
     current_time = get_current_time(action)
     current_time_dt = datetime.strptime(current_time, "%H:%M:%S")
     target_time_dt = datetime.strptime("7:00:00", "%H:%M:%S")
-    while current_time_dt <= target_time_dt:
+    while current_time_dt < target_time_dt:
       time.sleep(0.5)
       logging.info('sleep 0.5s')
       current_time = get_current_time(action)
